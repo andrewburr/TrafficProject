077d4b2f357b116c865c0234ca638dc9a8bdd850
CameronFleming77 <Cameron_Fleming77@hotmail.co.uk>
2017-03-14 14:33:55 +0000

    Add files via upload

---
+++ TrafficProject/Motorbike.h
@@ -0,0 +1,22 @@
+#ifndef MOTORBIKE_H
+#define MOTORBIKE_H
+#include "Vehicle.h"
+
+class Motorbike : public Vehicle
+{
+public:
+	// Getters
+	int getOrientation();
+
+	// Setters
+	void setOrientation(int);
+
+	Motorbike();
+	Motorbike(TimeClass*, int, int, int);
+	virtual ~Motorbike();
+protected:
+	int speed = 2;
+	int orientation;
+};
+
+#endif
