aaaa163e1e8823b6b89cd02aaeb25b42913c4d9e
andrewburr <andrew_burr@hotmail.co.uk>
2017-03-21 13:58:11 +0000

    Add files via upload

--- TrafficProject/main.cpp
+++ TrafficProject/main.cpp
@@ -5,8 +5,6 @@
 #include "Motorbike.h"
 #include "Road.h"
 #include "Pedestrian.h"
-#include "TJunction.h"
-#include "Crossroads.h"
 
 
 using namespace std;
@@ -14,21 +12,15 @@ using namespace std;
 int main()
 {
 	TimeClass globalTime;
-	Road testroads[5];
 
-	for (int i = 0; i < 5; i++)
-	{
-		testroads[i] =  Road("oneway", 1, 0, i);
-	}
-
-	TJunction test(&globalTime, &testroads[0], &testroads[1], &testroads[2], 0, 2);
+	Pedestrian test(2, 1, &globalTime);
 
 	globalTime.incrementTime();
 
-	cout << "Road : " << test.getRoad(0)->getType() << endl;
+	cout << "Pos : " << test.getPos_X() << " " << test.getPos_Y() << endl;
+	cout << "Time : " << test.getTime() << endl;
 
-
-	test.~TJunction();
+	
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	system("Pause");
 	return 0;
